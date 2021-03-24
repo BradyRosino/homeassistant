99471740f769f66a30f9dffa24e3243ce29057a0
Brady Rosino <brady@rosino.net>
2021-03-23 23:19:08 -0400

    Connect scenes to office Pico

--- appdaemon/apps/brady_office.py
+++ appdaemon/apps/brady_office.py
@@ -4,6 +4,7 @@ class BradyOffice(hass.Hass):
 
 	def initialize(self):
 		onkyo_task = None
+		scene_task = None
 		self.listen_event(self.lutron_event, "lutron_caseta_button_event", area_name="Brady Office", device_name="Pico")
 
 
@@ -17,13 +18,15 @@ class BradyOffice(hass.Hass):
 			return
 		
 		if data["button_number"] == 2:
-			self.call_service("media_player/turn_on", entity_id="media_player.brady_office_receiver")
+			self.create_task(self.set_scene("on"))
+			#self.call_service("media_player/turn_on", entity_id="media_player.brady_office_receiver")
 
 		elif data["button_number"] == 3:
 			self.call_service("media_player/toggle", entity_id="media_player.brady_office_receiver")
 		
 		elif data["button_number"] == 4:
-			self.call_service("media_player/turn_off", entity_id="media_player.brady_office_receiver")
+			self.create_task(self.set_scene("off"))
+			#self.call_service("media_player/turn_off", entity_id="media_player.brady_office_receiver")
 		
 		elif data["button_number"] == 5:
 			self.onkyo_task = self.create_task(self.onkyo_volume_adjust("media_player/volume_up"))
@@ -38,3 +41,18 @@ class BradyOffice(hass.Hass):
 			count = count + 1
 			await self.sleep(.3)
 			self.call_service(service,entity_id="media_player.brady_office_receiver")
+	
+	async def set_scene(self,scene):
+
+		if scene == "off":
+			self.call_service("light/turn_off", entity_id="light.brady_office_couch", transition=5)
+			self.call_service("light/turn_off", entity_id="light.brady_office_desk", transition=5)
+			return
+
+		if await self.sun_down():
+			self.call_service("light/turn_on", entity_id="light.brady_office_couch", brightness=255,transition=10,color_name="royalblue")
+			await self.sleep(3)
+			self.call_service("light/turn_on", entity_id="light.brady_office_desk", brightness=100,transition=7,kelvin=2700)
+		else:
+			self.call_service("light/turn_on", entity_id="light.brady_office_desk", brightness=255,transition=7,kelvin=3200)
+			self.call_service("light/turn_off", entity_id="light.brady_office_couch",transition=10)
