233f14d57a6b6349527c0db692862cec48ec28b7
Brady Rosino <brady@rosino.net>
2021-03-25 20:57:12 -0400

    Migrated emerson bedroom automations to Pyscript

--- pyscript/emerson_bedroom.py
+++ pyscript/emerson_bedroom.py
@@ -8,10 +8,13 @@ def door_event(value):
     task.unique("emerson_door_event")
 
     if value == "on":
+
         light.turn_on(entity_id="light.emerson_bedroom_nightstand",kelvin=3000,transition=3,brightness=75)
 
+        """Wait 30 minutes"""
         task.sleep(1800)
-        
+
+        """If still running, turn off the stuff"""
         light.turn_off(entity_id="light.emerson_bedroom_nightstand")
         switch.turn_off(entity_id="switch.emerson_bedroom_floor_fan")
 
@@ -21,3 +24,34 @@ def door_event(value):
         light.turn_off(entity_id="light.emerson_bedroom_nightstand",transition=3)
         task.sleep(3)
         light.turn_off(entity_id="light.emerson_bedroom_dresser",transition=5)
+
+#######################################
+# Emerson Ceiling Light Z-Wave Scenes
+#######################################
+
+@event_trigger("zwave_js_event", "node_id == 47")
+def zwave_event(**kwargs):
+
+    if kwargs["property_key"] == "003" and kwargs["value"] == "KeyPressed":
+        # FAVORITE / CONFIG BUTTON
+
+        light.turn_off(entity_id="light.emerson_bedroom_ceiling")
+        light.turn_on(entity_id="light.emerson_bedroom_nightstand", brightness=150, color_name="pink", transition=5)
+        light.turn_on(entity_id="light.emerson_bedroom_dresser", brightness=150, color_name="purple", transition=5)
+
+    elif data["property_key"] == "002" and (data["value"] == "KeyHeldDown" or data["value"] == "KeyPressed2x"):
+
+        # UP HELD or UP x2
+
+        light.turn_on(entity_id="light.emerson_bedroom_dresser", brightness=255, kelvin=2700, transition=3)
+        light.turn_on(entity_id="light.emerson_bedroom_nightstand", brightness=255, kelvin=2700, transition=3)
+        task.sleep(3)
+        light.turn_on(entity_id="light.emerson_bedroom_ceiling")
+
+
+    elif data["property_key"] == "001" and (data["value"] == "KeyHeldDown" or data["value"] == "KeyPressed2x"):
+
+        # DOWN HELD or DOWN x2
+        light.turn_off(entity_id="light.emerson_bedroom_nightstand", transition=3)
+        light.turn_off(entity_id="light.emerson_bedroom_dresser", transition=3)
+        light.turn_off(entity_id="light.emerson_bedroom_ceiling")
