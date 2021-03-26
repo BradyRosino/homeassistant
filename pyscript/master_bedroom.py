4d40fab607aa5597f53709c12c2fc4f3257c1ed8
Brady Rosino <brady@rosino.net>
2021-03-25 21:30:31 -0400

    Migrated bedroom automations to Pyscript

---
+++ pyscript/master_bedroom.py
@@ -0,0 +1,9 @@
+@event_trigger("sensor.master_bathroom_shower_humidity")
+def master_bathroom_humidity_changed(value):
+
+    if float(value) > 60.00:
+        if switch.master_bathroom_shower_fan == "off":
+            switch.turn_on(entity_id="switch.master_bathroom_shower_fan")
+    elif float(value) < 50:
+        if switch.master_bathroom_shower_fan == "on":
+            switch.turn_off(entity_id="switch.master_bathroom_shower_fan")
