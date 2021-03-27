966f498897be2e675142e366eeec98ec37bd46f0
Brady Rosino <brady@rosino.net>
2021-03-26 22:00:21 -0400

    Fixed humidity float

--- pyscript/master_bedroom.py
+++ pyscript/master_bedroom.py
@@ -4,6 +4,6 @@ def master_bathroom_humidity_changed(value):
     if float(value) > 60.00:
         if switch.master_bathroom_shower_fan == "off":
             switch.turn_on(entity_id="switch.master_bathroom_shower_fan")
-    elif float(value) < 50:
+    elif float(value) < 55.00:
         if switch.master_bathroom_shower_fan == "on":
             switch.turn_off(entity_id="switch.master_bathroom_shower_fan")
