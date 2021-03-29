19c5c51fdb8c66b81a3d16f344af1185511faac5
Brady Rosino <brady@rosino.net>
2021-03-29 07:00:35 -0400

    Fixed foyer motion light

--- pyscript/foyer.py
+++ pyscript/foyer.py
@@ -3,11 +3,10 @@ from datetime import timedelta
 
 @state_trigger("binary_sensor.foyer_motion == 'on'")
 @time_active("range(sunset - 20min, sunrise + 20min)")
-@task_unique("foyer_motion")
 def foyer_motion():
 
     if light.kitchen_island_lights == "off":
         last_changed = datetime.utcnow().replace(tzinfo=None) - datetime.fromisoformat(str(light.kitchen_island_lights.last_changed)).replace(tzinfo=None)
 
-        if last_changed < timedelta(minutes=2):
-            light.turn_on
+        if last_changed > timedelta(minutes=2):
+            light.turn_on(entity_id="light.kitchen_island_lights",brightness=75)
