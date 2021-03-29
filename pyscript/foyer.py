3da14e07a629a4671bed046841643d0bb1430719
Brady Rosino <brady@rosino.net>
2021-03-29 07:01:16 -0400

    Added fast light transition

--- pyscript/foyer.py
+++ pyscript/foyer.py
@@ -9,4 +9,4 @@ def foyer_motion():
         last_changed = datetime.utcnow().replace(tzinfo=None) - datetime.fromisoformat(str(light.kitchen_island_lights.last_changed)).replace(tzinfo=None)
 
         if last_changed > timedelta(minutes=2):
-            light.turn_on(entity_id="light.kitchen_island_lights",brightness=75)
+            light.turn_on(entity_id="light.kitchen_island_lights",brightness=75,transition=2)
