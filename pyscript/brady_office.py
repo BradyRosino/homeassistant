ec7baef9362452eaa9cd293f8da133ebcbfbb177
Brady Rosino <brady@rosino.net>
2021-04-13 11:42:49 -0400

    Remove volume adjustments on pico

--- pyscript/brady_office.py
+++ pyscript/brady_office.py
@@ -38,48 +38,6 @@ def brady_office_receiver_source_changed(**kwargs):
         media_player.volume_set(entity_id="media_player.brady_office_receiver",volume_level=0.3)
 
 
-#######################################
-# Handle Brady Office Pico Events
-#######################################
-
-@event_trigger("lutron_caseta_button_event", "area_name == 'Brady Office' and device_name == 'Pico'")
-def lutron_event(**kwargs):
-    
-    """Listen for button release to stop any running loops"""
-    if kwargs["action"] != 'press':
-        onkyo_volume_adjust("stop")
-        return
-    
-    """handle action"""
-
-    if kwargs["button_number"] == 2:
-        media_player.turn_on(entity_id="media_player.brady_office_receiver")
-    
-    elif kwargs["button_number"] == 4:
-        media_player.turn_off(entity_id="media_player.brady_office_receiver")
-
-    elif kwargs["button_number"] == 3:
-        media_player.toggle(entity_id="media_player.brady_office_receiver")
-    
-    elif kwargs["button_number"] == 5:
-        onkyo_volume_adjust("volume_up")
-
-    elif kwargs["button_number"] == 6:
-        onkyo_volume_adjust("volume_down")
-    
-    
-    
-def onkyo_volume_adjust(action):
-
-    task.unique('onkyo_volume_adjust')
-
-    if action == "volume_up" or action == "volume_down":
-        count = 0
-        while(count < 20):
-            count += 1
-            task.sleep(.3)
-            service.call("media_player",action, entity_id="media_player.brady_office_receiver")
-
 
 @state_trigger("sensor.brady_office_cube_action not in ['','wakeup']")
 def brady_office_cube_action(value):
