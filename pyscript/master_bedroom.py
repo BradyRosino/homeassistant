3754c367fbe6e81aac18a746b00fba090f6d547a
Brady Rosino <brady@rosino.net>
2021-06-18 12:56:25 -0400

    Moved more to out of box automations

--- pyscript/master_bedroom.py
+++
@@ -1,9 +0,0 @@
-@state_trigger("sensor.master_bathroom_shower_humidity")
-def master_bathroom_humidity_changed(value):
-
-    if float(value) > 60.00:
-        if switch.master_bathroom_shower_fan == "off":
-            switch.turn_on(entity_id="switch.master_bathroom_shower_fan")
-    elif float(value) < 55.00:
-        if switch.master_bathroom_shower_fan == "on":
-            switch.turn_off(entity_id="switch.master_bathroom_shower_fan")
